dc8f58c85bba13afa4b5779e43cb524c81858564
Alex Sarychev <freddis336@gmail.com>
2026-05-27 16:54:08 +0300

    Swapping image library for one that allows rotation

--- components/blocks/ImageUploadButton/ImageUploadButton.tsx
+++ components/blocks/ImageUploadButton/ImageUploadButton.tsx
@@ -3,62 +3,176 @@ import {useAppTheme} from '../../../hooks/useAppTheme';
 import {IconSymbol} from '../IconSymbol/IconSymbol';
 import {ThemedView} from '../ThemedView/ThemedView';
 import {Theme} from '../../../types/Colors';
-import {StyleSheet, Pressable, View, Alert, Image} from 'react-native';
+import {
+  StyleSheet,
+  Pressable,
+  View,
+  Alert,
+  Image,
+} from 'react-native';
 import {ThemedButton} from '../ThemedButton/ThemedButton';
-import {requestMediaLibraryPermissionsAsync, launchImageLibraryAsync} from 'expo-image-picker';
+import {
+  requestCameraPermissionsAsync,
+  requestMediaLibraryPermissionsAsync,
+} from 'expo-image-picker';
 import {ImageUploadButtonProps} from './types/ImageUploadButtonProps';
+import {openPicker, openCamera, Options} from 'react-native-image-crop-picker';
 
-const getStyles = (theme: Theme) => StyleSheet.create({
-  container: {
-    borderRadius: theme.borderRadiusS,
-    alignItems: 'center',
-    justifyContent: 'center',
-    alignSelf: 'flex-start',
-    width: '100%',
-    height: '100%',
-    overflow: 'hidden',
-  },
-});
+const getStyles = (theme: Theme) =>
+  StyleSheet.create({
+    container: {
+      borderRadius: theme.borderRadiusS,
+      alignItems: 'center',
+      justifyContent: 'center',
+      alignSelf: 'flex-start',
+      width: '100%',
+      height: '100%',
+      overflow: 'hidden',
+    },
+  });
 
 export const ImageUploadButton: FC<ImageUploadButtonProps> = (props) => {
   const theme = useAppTheme();
   const styles = getStyles(theme);
+
   const [src, setSrc] = useState<string | null>(props.value);
-  const onPress = async () => {
-    const permissionResult = await requestMediaLibraryPermissionsAsync();
+
+  const handleResult = (result: string) => {
+    props.onChange?.(result);
+    setSrc(result);
+    setSrc(`data:image/jpeg;base64,${result}`);
+  };
+
+  const openCameraOnPress = async () => {
+    const permissionResult = await requestCameraPermissionsAsync();
+
     if (!permissionResult.granted) {
-      Alert.alert('Permission required', 'Permission to access the media library is required.');
+      Alert.alert(
+        'Permission required',
+        'Permission to access the camera is required.',
+      );
       return;
     }
-    const result = await launchImageLibraryAsync({
-      mediaTypes: ['images'],
-      allowsEditing: true,
-      base64: true,
-      selectionLimit: 1,
-    });
-    if (!result.canceled && result.assets[0]?.base64) {
-      props.onChange?.(result.assets[0].base64);
-      setSrc(`data:image/jpeg;base64,${result.assets[0].base64}`);
+
+    const options: Options = {
+      mediaType: 'photo',
+      cropping: true,
+      width: 1500,
+      height: 1500,
+      forceJpg: true,
+      compressImageMaxWidth: 1500,
+      compressImageMaxHeight: 1500,
+      includeBase64: true,
+    };
+    const cameraResult = await openCamera(options);
+    if (!cameraResult.data) {
+      return;
     }
+    handleResult(cameraResult.data);
+    // handleResult(result);
   };
+
+  const openGallery = async () => {
+    const permissionResult =
+      await requestMediaLibraryPermissionsAsync();
+
+    if (!permissionResult.granted) {
+      Alert.alert(
+        'Permission required',
+        'Permission to access the media library is required.',
+      );
+      return;
+    }
+    const options: Options = {
+      mediaType: 'photo',
+      cropping: true,
+      width: 1500,
+      height: 1500,
+      forceJpg: true,
+      compressImageMaxWidth: 1500,
+      compressImageMaxHeight: 1500,
+      includeBase64: true,
+    };
+    const result = await openPicker(options);
+
+    if (!result.data) {
+      return;
+    }
+    handleResult(result.data);
+  };
+
+  const onPress = async () => {
+    Alert.alert(
+      'Select Image',
+      'Choose image source',
+      [
+        {
+          text: 'Camera',
+          onPress: openCameraOnPress,
+        },
+        {
+          text: 'Gallery',
+          onPress: openGallery,
+        },
+        {
+          text: 'Cancel',
+          style: 'cancel',
+        },
+      ],
+    );
+  };
+
   const onRemovePress = () => {
     if (!props.onRemove) {
       return;
     }
+
     setSrc(null);
     props.onRemove();
   };
+
   return (
     <>
-      <Pressable onPress={onPress} style={[props.style]} className={props.className}>
+      <Pressable
+        onPress={onPress}
+        style={[props.style]}
+        className={props.className}>
         <ThemedView style={styles.container}>
-        {!props.value && (
-          <View style={{padding: theme.paddingM, width: '50%', height: '50%', alignItems: 'center', justifyContent: 'center'}}>
-            <IconSymbol name="photo.badge.plus" size={50} color={theme.surfaceText} />
-          </View>
+          {!src && (
+            <View
+              style={{
+                padding: theme.paddingM,
+                width: '50%',
+                height: '50%',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+              <IconSymbol
+                name="photo.badge.plus"
+                size={50}
+                color={theme.surfaceText}
+              />
+            </View>
           )}
-        {src && <Image src={src} style={{height: '100%', width: '100%', resizeMode: 'cover'}} />}
-        {props.value && props.onRemove && <ThemedButton style={{position: 'absolute', bottom: 10}} onPress={onRemovePress}>Remove</ThemedButton>}
+
+          {src && (
+            <Image
+              source={{uri: src}}
+              style={{
+                height: '100%',
+                width: '100%',
+                resizeMode: 'cover',
+              }}
+            />
+          )}
+
+          {src && props.onRemove && (
+            <ThemedButton
+              style={{position: 'absolute', bottom: 10}}
+              onPress={onRemovePress}>
+              Remove
+            </ThemedButton>
+          )}
         </ThemedView>
       </Pressable>
     </>
